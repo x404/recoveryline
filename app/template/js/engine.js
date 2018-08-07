fb0e90f7b940d4573ce39ed2e871ce431a2af596
x404 <kronos2003@gmail.com>
2018-08-07 21:24:00 +0300

    first commit

---
+++ app/template/js/engine.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+
+	// карусель
+	$('#foo1').owlCarousel({
+		loop:false,
+		nav:true,
+		dots: false,
+		items:3,
+		startPosition : 2,
+		stagePadding : 250,
+		navText: ["", ""],
+		onInitialized: function (event) {
+			refreshFirstLastVisible(event);
+		},
+		onChanged: function (event) {
+			refreshFirstLastVisible(event);
+		},
+		responsive:{
+			0:{
+				items:1,
+				stagePadding: 20
+			},
+			900:{
+				items:2,
+				stagePadding: 0
+			},
+			992:{
+				items:1
+			},
+			1250:{
+				items:2
+			},
+			1550:{
+				items:3
+			}
+		}
+	});
+
+
+	// mobile-menu
+	$('#navbar').each(function(){
+		var $this = $(this),
+			$link = $('.navbar-toggle'),
+			$close = $('.close-menu'),
+
+			init = function(){
+				$link.on('click', openMenu);
+				$close.on('click', closeMenu);
+			},
+			openMenu = function(e){
+				e.preventDefault();
+				h = $(document).height();
+				$('body').addClass('o-menu');
+				$('#navbar').height(h);
+
+			},
+			closeMenu = function(e){
+				e.preventDefault();
+				$('body').removeClass('o-menu');
+				$('#navbar').height('auto');
+			};
+		init();
+	});	
+});
