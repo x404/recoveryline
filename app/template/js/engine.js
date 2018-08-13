e3f1bdccc6ce99cd841e1ad6c6bad54ad2ea8516
x404 <kronos2003@gmail.com>
2018-08-13 11:45:44 +0300

    Добавлен слайдер на главной

--- app/template/js/engine.js
+++ app/template/js/engine.js
@@ -1,41 +1,13 @@
 $(document).ready(function(){
-
-	// карусель
-	$('#foo1').owlCarousel({
-		loop:false,
-		nav:true,
-		dots: false,
-		items:3,
-		startPosition : 2,
-		stagePadding : 250,
-		navText: ["", ""],
-		onInitialized: function (event) {
-			refreshFirstLastVisible(event);
-		},
-		onChanged: function (event) {
-			refreshFirstLastVisible(event);
-		},
-		responsive:{
-			0:{
-				items:1,
-				stagePadding: 20
-			},
-			900:{
-				items:2,
-				stagePadding: 0
-			},
-			992:{
-				items:1
-			},
-			1250:{
-				items:2
-			},
-			1550:{
-				items:3
-			}
-		}
+	$('#home-carousel').slick({
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: false,
+		autoplaySpeed: 4000,
+		adaptiveHeight: true,
+		arrows : false
 	});
-
 
 	// mobile-menu
 	$('#navbar').each(function(){
@@ -61,4 +33,12 @@ $(document).ready(function(){
 			};
 		init();
 	});	
+
+	$('#totop').click(function (){
+		$('body, html').animate({
+			scrollTop:0
+		}, 800);
+		return false;
+	});
+
 });
