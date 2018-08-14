68470cf31e6118be878f51ddf92dc6e923ff3f22
x404 <kronos2003@gmail.com>
2018-08-14 12:25:24 +0300

    Добавлено всплывающее окно "обратный звонок"

--- app/template/js/engine.js
+++ app/template/js/engine.js
@@ -56,3 +56,18 @@ $(document).ready(function(){
 	});
 
 });
+	
+
+$(function(){
+	$('.policy input').click(function(){
+		var $this = $(this),
+			$submit = $this.closest('.form-policy');
+
+		if ($this.is(':checked')){
+			$submit.find('.input, .form-control, .submit, textarea, input[type=radio]').removeAttr('disabled');
+		} else {
+			$submit.addClass('disabled');
+			$submit.find('.input, .form-control, .submit, textarea, input[type=radio]').attr('disabled', true);
+		}
+	})
+});
--- gulpfile.js
+++ gulpfile.js
@@ -59,6 +59,7 @@ gulp.task('compress', function(){
 				config.libsDir + '/jquery-validation/dist/jquery.validate.js',
 				config.templateDir + '/js/util.js',
 				config.templateDir + '/js/tab.js',
+				config.templateDir + '/js/modal.js',
 				config.templateDir + '/js/popper.min.js',
 				config.templateDir + '/js/jquery.fancybox.min.js'
 			]),
