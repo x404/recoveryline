9454220a89f0980955a472b77a1ca7dcd1479eff
x404 <kronos2003@gmail.com>
2018-08-14 13:13:54 +0300

    Добавлено всплывающее окно "Обратный звонок"

--- gulpfile.js
+++ gulpfile.js
@@ -57,10 +57,10 @@ gulp.task('compress', function(){
 			gulp.src([  // Берем все необходимые библиотеки
 				config.libsDir + '/jquery/dist/jquery.js',
 				config.libsDir + '/jquery-validation/dist/jquery.validate.js',
+				config.templateDir + '/js/popper.min.js',
 				config.templateDir + '/js/util.js',
 				config.templateDir + '/js/tab.js',
-				config.templateDir + '/js/modal.js',
-				config.templateDir + '/js/popper.min.js',
+				// config.templateDir + '/js/modal.js',
 				config.templateDir + '/js/jquery.fancybox.min.js'
 			]),
 			concat('libs.min.js'), // Собираем их в кучу в новом файле libs.min.js
